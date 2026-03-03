2b68ee6b742aefc4dfdde8e6683478b34922faa1
Hans Oersted <zheshen@ieee.org>
2026-03-03 23:11:26 +0800

    all from e only

--- initial_csv_process.py
+++ initial_csv_process.py
@@ -1,13 +1,30 @@
+import numpy as np
 import pandas as pd
 
 dt = 0.007999999999995566
 
 df = pd.read_csv("altitude_pairs.csv")
 
-df_new = pd.DataFrame()
-df_new["e"] = df["e"]
-df_new["de"] = df["edot"]
+e = df["e"].to_numpy(dtype=np.float64)
+N = len(e)
+if N < 3:
+    raise ValueError(f"Need at least 3 samples to compute de and dde. Got N={N}")
 
-df_new["dde"] = (df["edot_next"] - df["edot"]) / dt
+# de: forward difference
+de = (e[1:] - e[:-1]) / dt          # length N-1
+
+# dde: forward difference of de
+dde = (de[1:] - de[:-1]) / dt       # length N-2
+
+# N-2
+e_aligned = e[:-2]                  # e[0 .. N-3]
+de_aligned = de[:-1]                # de[0 .. N-3]
+dde_aligned = dde                   # dde[0 .. N-3]
+
+df_new = pd.DataFrame({
+    "e": e_aligned,
+    "de": de_aligned,
+    "dde": dde_aligned
+})
 
 df_new.to_csv("altitude_processed.csv", index=False)
